1dce7c4437b30011ff70c25e2f14b63ed859c9cd
Jorge-Chinchilla <38166459+Jorge-Chinchilla@users.noreply.github.com>
2020-12-13 05:40:43 -0600

    Analizador Lexico en C++
    Estructura principal del programa.
    Se agregaron las variables globales, palabras reservadas, lectura/escritura de los archivos y un poco de logica.
    Nota: Las funciones discutidas estan comentadas.

--- Analizador lexico/AnalizadorLexico.cpp
+++ Analizador lexico/AnalizadorLexico.cpp
@@ -3,30 +3,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-//prototipo de funciones
-void analizador(); //funcion que cambia/analiza los estados o verifica errores
-void Estado();  //verifica al estado que pertenece
-void buscar();  //compara identificador con la tabla de simbolos, nos dice si ecuentra o no
+//Funciones momentaneamente comentadas
+//void AnalizadorDeCaracter(char car);// Esta funcion se encarga del leer caracter a caracter las palabras para asignarle un estado 
+//void Estados();//Esta funcion manda los estados de los caracteres leidos
+//void BuscarReservadas();//Esta funcion se encarga de buscar entre el arreglo de palabras reservadas
 
-//Variables globales
-char *tablaDeSimbolos[] = {"definir, enceder, repetir, mientras, finrepetir, apagar"};
-char identificador[99];
-int tamIndicador=sizeof(tablaDeSimbolos)/sizeof(char *); //devuelve el tamaño en bytes que abarca la variable
-int i; //manejo de ciclos
-int estado=0; //el entero que asignara el estado
-FILE entradaF;
-FILE salidaF;
+const char *reservadas[] = {"encender","apagar","inicio","definir","repetir","fin","mientras","si","sino"}; //arreglo de palabras reservadas
 
-//contadores
-int variablesCont=0;
-int encenderCont=0;
-int apagarCont=0;
-int cicloCont=0;
-int aritmeticaCont=0;
+int IndicadorTam=sizeof(reservadas)/sizeof(char *);//El tamano de nuestras las reservadas y el tamano de memoria de el char
+int i; //Auxiliar para los ciclos
+char identificador[150]; //un identificador para auxiliar la asignaciÃ³n de estados
+char tmp[2]; //temporal para guardar caracteres de una cadena
+int estado=0; //El estado es un entero el cual representa el valor de lo encontrado. Ejemp: Variable, reservada, simbolo, etc
 
+//Estos son los contadores de los elementos de la tabla
+int contadorVariables=0;
+int contadorNumeros=0;
+int contadorPalabrasReservadas=0;
+int contadorSimbolos=0;
+int contadorSignos=0;
+int contadorEncender=0;
+int contadorApagado=0;
+int contadorCiclos=0;
 
 int main(){
-
-    int a = 0;
-
+	FILE *archivoE; //Archivo de entrada (Incluye el lenguaje a analizar)
+	FILE *archivoS; //Archivo de salida (Donde se imprime la tabla)
+   
+	printf("---- ANALIZADOR LEXICO en C++ ---\n");
+	printf("Preparando archivo.....\n");
+   
+	archivoE=fopen("entrada.txt", "r" ); //Abrimos el archivo que vamos a leer
+	archivoS=fopen("salida.txt", "wt"); //Preparamos el archivo de escritura
+   
+	char caracter;
+	//Validamos si el archivo existe
+    if ( archivoE == NULL ) {
+		printf ( "El archivo no fue encontrado. \n" ) ;
+     	return 1;
+   	}
+    printf( "Leyendo entrada.txt....\n" ) ;
+	printf( "Archivo list....\n" ) ;
+	
+   	while (1){
+	   //fgetch toma el texto del archivo y itera caracter a caracter
+	  	caracter=fgetc(archivoE);
+	  	if(caracter==EOF){ //EOF:End Of File
+	  	break;
+	  	}
+	  	//Verificamos que el programa empieza con la palabra reservada "inicio"
+	  	while(i==0){
+		   	if(caracter!='i'&&caracter!='n'&&caracter!='i'&&caracter!='c'&&caracter!='i'&&caracter!='o'){
+			   	printf("\n\nError, No se encontro el inicio");
+		   		exit(-1);
+			}
+		   	i=1;
+	  	}
+	  
+		//analiza nuestros caracteres mas generales aun no esta construido
+		//AnalizadorDeCaracter(caracter);
+   	}
+ 
+			
 }
